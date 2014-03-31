599641e8bba7ff276034b5d0f4e0769769d8528d
Cristian Pop <cristian.pop3009@gmail.com>
2014-03-31 20:44:44 +0300

    Added assertions and javadoc comments.

--- src/Transfer.java
+++ src/Transfer.java
@@ -3,30 +3,54 @@
  *
  */
 public class Transfer {
+	/*
+	 * 
+	 */
 	private File file;
+	/*
+	 * 
+	 */
 	private Peer peer;
+	/*
+	 * 
+	 */
 	private int progress;
 	
+	/*
+	 * 
+	 */
 	public Transfer(File file, Peer peer) {
 		this.file = file;
 		this.peer = peer;
+		this.progress = 0;
 	}
 	
+	/*
+	 * 
+	 */
 	public File getFile() {
 		return file;
 	}
 	
+	/*
+	 * 
+	 */
 	public Peer getPeer() {
 		return peer;
 	}
 
+	/*
+	 * 
+	 */
 	public int getProgress() {
 		return progress;
 	}
 
+	/*
+	 * 
+	 */
 	public void setProgress(int progress) {
+		assert progress >= 0 && progress <= 100;
 		this.progress = progress;
 	}
-	
-	
 }
