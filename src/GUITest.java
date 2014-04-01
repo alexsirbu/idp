f6313a0f9db4e556aa28c3744ae2eed3d79d9888
CristianPop <cristian.pop3009@gmail.com>
2014-04-01 14:20:19 +0300

    Removed junit lib.

---
+++ src/GUITest.java
@@ -0,0 +1,22 @@
+/*
+ * 
+ */
+public class GUITest {
+	/*
+	 * 
+	 */
+	public GUITest() {
+	}
+	
+	protected void setUp() {
+		
+	}
+	
+	protected void tearDown() {
+		
+	}
+	
+	public void testAddPeer() {
+		
+	}
+}
