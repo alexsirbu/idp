1b206d10fe30d844bcbfe8c3078e24cfba63aa5c
AlexS <alexandru.sirbu@hostway.ro>
2014-04-16 22:53:41 +0300

    stated support for log4j

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
+import org.apache.log4j.*;
 
 /*
  * 
  */
 public class Main {
+	static Logger logger = Logger.getLogger(Main.class);
+	
 	/*
 	 * 
 	 */
@@ -16,7 +19,17 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		if (args.length < 1)
 			throw new Exception("Not enough parameters given.\nParameters required: user_name");
-			
+		
+		FileAppender appender = new FileAppender();
+		appender.setName("A2");
+		appender.setFile(args[0]+".log");
+		appender.setThreshold(Level.ALL);
+	    appender.setLayout(new PatternLayout("%d %-5p [%c{1}] %m%n"));
+	    appender.activateOptions();
+		logger.addAppender(appender);
+		logger.debug("MARIETA");
+		logger.error("PLM");
+		
 		Mediator mediator = new Mediator(args[0]);
 		
 		GUI gui = new GUI(mediator);
