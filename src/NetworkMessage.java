63227925055b91d49b168ca3b269af49533f20e6
AlexS <alexandru.sirbu@hostway.ro>
2014-04-17 17:57:35 +0300

    modified network message

--- src/NetworkMessage.java
+++ src/NetworkMessage.java
@@ -2,26 +2,33 @@ import java.nio.ByteBuffer;
 
 public class NetworkMessage {
 
-	int messageSize;
 	Message message;
+	boolean complete;
 	
-	public NetworkMessage(int messageSize, Message message)
+	public NetworkMessage(Message message)
 	{
-		this.messageSize = messageSize;
 		this.message = message;
+		this.complete = true;
 	}
 	
 	public NetworkMessage(byte[] encodedMessage)
 	{
 		ByteBuffer buffer = ByteBuffer.wrap(encodedMessage);
 		
-		messageSize = buffer.getInt();
-		byte[] messageBytes = new byte[messageSize];
-		
-		for(int i=0; i<messageSize; i++)
-			messageBytes[i]=buffer.get();
-		
-		message = MessageFactory.decodeMessage(messageBytes);
+		int messageSize = buffer.getInt();
+		if (buffer.remaining() != messageSize)
+			complete=false;
+		else
+		{
+			byte[] messageBytes = new byte[messageSize];
+			
+			for(int i=0; i<messageSize; i++)
+				messageBytes[i]=buffer.get();
+			
+			message = MessageFactory.decodeMessage(messageBytes);
+			
+			complete = true;
+		}
 	}
 	
 	public byte[] encode()
@@ -41,5 +48,9 @@ public class NetworkMessage {
 		return message;
 	}
 	
-		
+	public boolean getComplete()
+	{
+		return complete;
+	}
+	
 }
